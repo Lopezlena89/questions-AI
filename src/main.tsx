a15dee8d239a1c3460f86051f79fe10cabd6bd46
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-02-04 21:54:25 -0600

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
 import "./index.css";
-import { AppRouter } from './AppRouter';
+// import { AppRouter } from './AppRouter';
+import { App } from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       
-        {/* <App/> */}
-        <AppRouter/>
+        <App/>
+        {/* <AppRouter/> */}
       
      
     </Provider>
